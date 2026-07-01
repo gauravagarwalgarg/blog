609206d7b652e4d340ae351ba6ca60e053acff12
Gaurav Agarwal <gauravagarwalgarg@gmail.com>
2026-07-02 01:23:05 +0530

    fix: remove circle dot, fix titles, update RSS for Google Feeds
    - Remove .category-dot circle from article cards
    - Technical Articles & Blog -> Articles & Blog
    - Technical Concepts -> Concepts
    - Update hero description to broader blend (no technical repetition)
    - Fix RSS feed: add author, categories, atom:link, language for Google Feeds
    - Remove Technical from concept page title

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,29 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { SITE_DESCRIPTION, SITE_TITLE, SITE_AUTHOR } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog'))
+		.filter(p => !p.data.draft)
+		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
+		language: 'en-us',
+		managingEditor: `gauravagarwalgarg@gmail.com (${SITE_AUTHOR})`,
+		webMaster: `gauravagarwalgarg@gmail.com (${SITE_AUTHOR})`,
+		copyright: `Copyright ${new Date().getFullYear()} ${SITE_AUTHOR}`,
+		lastBuildDate: new Date(),
 		items: posts.map((post) => ({
-			...post.data,
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.pubDate,
 			link: `/posts/${post.id}/`,
+			categories: [post.data.category, ...(post.data.tags || [])],
+			author: `gauravagarwalgarg@gmail.com (${SITE_AUTHOR})`,
 		})),
+		customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" xmlns:atom="http://www.w3.org/2005/Atom"/>`,
 	});
 }
